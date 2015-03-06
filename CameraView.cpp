4520ad252f0534e1aa4aee1d5ee28b69ac52540e
Kevin Chow <kevin_c@telus.net>
2015-03-05 23:41:49 -0800

    Increased functionality -> Still highly experimental

--- CameraView.cpp
+++ CameraView.cpp
@@ -3,29 +3,20 @@
 
 #include "CameraView.hpp"
 
-CameraView::CameraView(int _xWindowDimension, int _yWindowDimension) :  m_viewpoint(0,0),
-                                                                        m_window_dimensions(_xWindowDimension, _yWindowDimension)
+#include <iostream>
+
+CameraView::CameraView(int _xWindowDimension, int _yWindowDimension, int x, int y) : m_window_dimensions(_xWindowDimension, _yWindowDimension),
+                                                                                     m_viewpoint(x, y)
 {
-    xWindowDimension = _xWindowDimension;
-    yWindowDimension = _yWindowDimension;
-
     m_zoom_level = 3;
-}
-
-CameraView::CameraView(int _xWindowDimension, int _yWindowDimension, int x, int y) : m_viewpoint(x, y),
-                                                                                    m_window_dimensions(_xWindowDimension, _yWindowDimension)
-{
-    xWindowDimension = _xWindowDimension;
-    yWindowDimension = _yWindowDimension;
-
-    m_zoom_level = 3;
+    m_scale_level = 1;
 }
 
 //TODO: Set std::min values as well
 void CameraView::changeView(bool up, bool down, bool left, bool right)
 {
     m_viewpoint.x = std::max(m_viewpoint.x + SCROLL_SPEED * right - SCROLL_SPEED * left, 0);    //
-    m_viewpoint.y = std::max(m_viewpoint.y + SCROLL_SPEED * down  - SCROLL_SPEED * up, static_cast<int>(pow(2, m_zoom_level - 3)*(512 * -16 - 16*8)));  //return std::min eventually
+    m_viewpoint.y = std::max(m_viewpoint.y + SCROLL_SPEED * down  - SCROLL_SPEED * up, static_cast<int>(m_scale_level*(512 * -16 - 16*8)));  //return std::min eventually
 }
 
 void CameraView::setDeltaView(int x, int y)
@@ -48,18 +39,16 @@ void CameraView::setZoomLevel(int _zoom_level)
             m_viewpoint.x -= m_window_dimensions.x / 4;
             m_viewpoint.y -= m_window_dimensions.y / 4;
         }
-        m_zoom_level = _zoom_level;
+        m_zoom_level  = _zoom_level;
+        m_scale_level = pow(2, m_zoom_level - 3);
 
         // Keep in bounds
         m_viewpoint.x = std::max(m_viewpoint.x, 0);
-        m_viewpoint.y = std::max(m_viewpoint.y , static_cast<int>(pow(2, m_zoom_level - 3)*(512 * -16 - 16*8)));  //return std::min eventually
+        m_viewpoint.y = std::max(m_viewpoint.y , static_cast<int>(m_scale_level*(512 * -16 - 16*8)));  //return std::min eventually
 
 }
 
 void CameraView::setWindowDimensions(int _xWindowDimension, int _yWindowDimension)
 {
-    xWindowDimension = _xWindowDimension;
-    yWindowDimension = _yWindowDimension;
-
     m_window_dimensions = {_xWindowDimension, _yWindowDimension};
 }
--- Tilemap.cpp
+++ Tilemap.cpp
@@ -22,7 +22,7 @@ void Tilemap::load(const std::string &_tileset, const World &_game_world, const 
 
     //Placeholder values
 
-    //Used for linear transformation math
+    // Used for calculating transformation matrices of x any y values
     int x1, y1;
     //Used to store TileType enum (from Basetile.hpp)
     int tile_type;
@@ -32,12 +32,22 @@ void Tilemap::load(const std::string &_tileset, const World &_game_world, const 
     sf::Vector2i vertex_pos;
     //Used to point to 4 values in an array (0, 1, 2, 3) of quads
     sf::Vertex* quad;
+    //
+    sf::Vector2i tilesFitOnscreen(_camera.getResolution().x / static_cast<int>(64 * _camera.getScale()), _camera.getResolution().y / static_cast<int>(32 * _camera.getScale()));
+
+    std::cout << _camera.getResolution().y / static_cast<int>(32 * _camera.getScale()) << std::endl;
+
+    // The "x" size of the game map
+    // The "y" size of the game map;
+    // Will probably take this out eventually for optimization or something
+    int   x_size = _game_world.getXsize(),
+          y_size = _game_world.getYsize();
 
     // The x and y solutions to the inverse transformation matrix [x] [ 1/64 -1/32]
     //                                                            [y] [ 1/64  1/32]
-    // With each term multiplied by the reciprocal of m_scale_level (by theorem of inverse matrices)
-    int xTileTopLeft = static_cast<int>(_camera.getX()/64 - _camera.getY()/32);
-    int yTileTopLeft = static_cast<int>(_camera.getX()/64 + _camera.getY()/32);
+    // With each term multiplied by the reciprocal of _camera.getScale() (by theorem of inverse matrices)
+    int xTileTopLeft = static_cast<int>((_camera.getX()/64 - _camera.getY()/32) / _camera.getScale());
+    int yTileTopLeft = static_cast<int>((_camera.getX()/64 + _camera.getY()/32) / _camera.getScale());
 
     // Precalculate the start and stop points
     // BEST FIX:
@@ -45,12 +55,13 @@ void Tilemap::load(const std::string &_tileset, const World &_game_world, const 
     //      THIS WILL BE A DRASTIC CHANGE AND WILL REQUIRE USING THE FOR LOOPS.  IS IT WORTH IT? -> MAY IMPROVE PERFORMANCE
     // SECONDARY FIX:
     // TODO Fix height offset values ->WILL HURT PERFORMANCE ACTUALLY
-    int xTileStart = std::max(static_cast<int>(xTileTopLeft - 22 - 12), 0);
-    int xTileEnd   = std::min(m_map_dimensions.x, xTileTopLeft + 30);
+    int xTileStart = std::max(static_cast<int>(xTileTopLeft - tilesFitOnscreen.y - 4), 0);
+    int xTileEnd   = std::min(x_size, xTileTopLeft + tilesFitOnscreen.x);
 
     int yTileStart = std::max(yTileTopLeft, 0);
-    int yTileEnd   = std::min(m_map_dimensions.y, static_cast<int>(yTileTopLeft + 22 + 30 + 12));
+    int yTileEnd   = std::min(y_size, static_cast<int>(yTileTopLeft + tilesFitOnscreen.y + tilesFitOnscreen.x + 6));
 
+    //Populate VertexArray with tiles from the world
     for (int i = xTileStart; i < xTileEnd; i++){
 
         for (int j = yTileStart; j < yTileEnd; j++){
@@ -60,34 +71,53 @@ void Tilemap::load(const std::string &_tileset, const World &_game_world, const 
             if (tile_type == 0)
                 text_pos = {0,0};
             else
-                text_pos = {((tile_type - 1) % 4) * 64,
-                            ((tile_type - 1) / 4 + 1) * 48};
+                text_pos = {((tile_type - 1) % 4)     * static_cast<int> (64 * _camera.getScale()),
+                            ((tile_type - 1) / 4 + 1) * static_cast<int> (48 * _camera.getScale())};
 
             //set up a pointer to 4 vertices given the coordinates given
-            quad = &m_maptile_verts[((i - xTileStart) + (j - yTileStart) * m_map_dimensions.x) * 4];
+            quad = &m_maptile_verts[((i - xTileStart) + (j - yTileStart) * x_size) * 4];
 
-            //Perform linear transformation to pixel domain
-            x1 = 32 * ( i + j);
-            y1 = 16 * (-i + j);
+            // The x and y solutions to the transformation matrix [x] [ 32      32 ]
+            //                                                    [y] [-16      16 ]
+            x1 =                 (_camera.getScale() * 32) * ( i + j);
+            y1 = static_cast<int>(_camera.getScale() * 16) * (-i + j);
 
-            vertex_pos = {x1 - _camera.getX(), y1 - _camera.getY() - 8 * _game_world.tile(i,j).referenceHeight()};
+            //The exact position of the referenced basetile, accounting for transformation, camera poeision, and tile height
+            vertex_pos = {x1 - _camera.getX(), static_cast<int>(y1 - _camera.getY() - 8 * _game_world.tile(i,j).referenceHeight() * _camera.getScale())};
 
             quad[0].position  = sf::Vector2f(vertex_pos);
-            quad[1].position  = sf::Vector2f(vertex_pos.x + 64, vertex_pos.y);
-            quad[2].position  = sf::Vector2f(vertex_pos.x + 64, vertex_pos.y + 48);
-            quad[3].position  = sf::Vector2f(vertex_pos.x     , vertex_pos.y + 48);
+            quad[1].position  = sf::Vector2f(vertex_pos.x + 64 * _camera.getScale(), vertex_pos.y);
+            quad[2].position  = sf::Vector2f(vertex_pos.x + 64 * _camera.getScale(), vertex_pos.y + 48 * _camera.getScale());
+            quad[3].position  = sf::Vector2f(vertex_pos.x     , vertex_pos.y + 48 * _camera.getScale());
 
             quad[0].texCoords = sf::Vector2f(text_pos);
-            quad[1].texCoords = sf::Vector2f(text_pos.x + 64, text_pos.y);
-            quad[2].texCoords = sf::Vector2f(text_pos.x + 64, text_pos.y + 48);
-            quad[3].texCoords = sf::Vector2f(text_pos.x,      text_pos.y + 48);
+            quad[1].texCoords = sf::Vector2f(text_pos.x + 64 * _camera.getScale(), text_pos.y);
+            quad[2].texCoords = sf::Vector2f(text_pos.x + 64 * _camera.getScale(), text_pos.y + 48 * _camera.getScale());
+            quad[3].texCoords = sf::Vector2f(text_pos.x,      text_pos.y + 48 * _camera.getScale());
 
         }
     }
 }
 
+
 void Tilemap::draw(sf::RenderTarget &_target, sf::RenderStates _states) const
 {
     _states.texture = &m_maptile_tex;
     _target.draw(m_maptile_verts, _states);
+}
+
+//TODO: Use better numbers i.e. actually calculate an algorithm
+void Tilemap::zoom(int _zoom_level)
+{
+    m_maptile_verts.clear();
+
+    switch(_zoom_level) {
+    case 4:
+        m_maptile_verts.resize(m_map_dimensions.x * m_map_dimensions.y * 2);
+    case 5:
+        m_maptile_verts.resize(m_map_dimensions.x * m_map_dimensions.y * 1);
+    default:
+        m_maptile_verts.resize(m_map_dimensions.x * m_map_dimensions.y * 4);
+
+    }
 }
